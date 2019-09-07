0eb1b995fbc20bafe30976ab396450a4520b1ebf
shankyjs <jhancarlossilva.c@gmail.com>
2019-09-07 01:09:49 -0600

    ad

---
+++ Module 3/cajero_automatico/js/classes.js
@@ -0,0 +1,20 @@
+class billetes
+{
+    constructor(img, valor, cantidad)
+    {
+        this.img = new Image();
+        this.valor = valor;
+        this.cantidad = cantidad;
+        this.img.src = images[this.valor];
+    }
+}
+
+var images = [];
+images[10] = "assets/10.jpg";
+images[20] = "assets/20.jpg";
+images[50] = "assets/50.jpg";
+
+var coleccion = [];
+coleccion.push(new billetes(50,50,3));
+coleccion.push(new billetes(20,20,3));
+coleccion.push(new billetes(50,50,3));
